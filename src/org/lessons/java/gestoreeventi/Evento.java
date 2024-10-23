207a2dde529f92fb4f49a0f7c829461fa66de9b3
AndreaOffredi <info@andreaoffredi.com>
2024-10-23 10:06:21 +0200

    Metodi Prenota - Disdici - override toString()

--- src/org/lessons/java/gestoreeventi/Evento.java
+++ src/org/lessons/java/gestoreeventi/Evento.java
@@ -1,5 +1,6 @@
 package org.lessons.java.gestoreeventi;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 
 public class Evento {
@@ -13,7 +14,7 @@ public class Evento {
 	//Costruttore che richieda parametri e inizializzi a 0 posti prenotati
 	public Evento(String titoloEvento, LocalDate dataEvento, int postiTotali) {
 		if (dataEvento.isBefore(LocalDate.now())) {
-            throw new IllegalArgumentException("La data dell'evento non può essere nel passato.");
+            throw new IllegalArgumentException("La data insertia è già passata, inserisci una nuova data futura.");
         }
         if (postiTotali <= 0) {
             throw new IllegalArgumentException("Il numero di posti totali deve essere positivo.");
@@ -50,7 +51,41 @@ public class Evento {
 	}
 	
 	//Metodo per la Prenotazione dell'evento
+	public void prenotazionePosti() {
+		if(LocalDate.now().isAfter(dataEvento)) {
+			System.out.println("L'evento è già passato.");
+			return;
+		}else if (postiPrenotati >= postiTotali) {
+			System.out.println("L'evento è tutto SoldOut.");
+			return;
+		}else {
+			postiPrenotati++;
+			System.out.println("Prenotazione avvenuta. Posti prenotati: " + postiPrenotati);
+		}
+	}
 	//Metodo per la Cancellazione dell'evento
+	public void cancellazionePosti() {
+		if(LocalDate.now().isAfter(dataEvento)) {
+			System.out.println("L'evento è già passato.");
+			return;
+		}else if (postiPrenotati <= 0) {
+			System.out.println("Non ci sono prenotazioni al momento.");
+			return;
+		}else {
+			postiPrenotati--;
+			System.out.println("Disdetta avvenuta. Posti prenotati: " + postiPrenotati);
+		}
+	}
+	//Overrade del metodo ToString
+	@Override
+	public String toString() {
+		//definisco il formato della data
+		SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormattata = formatoData.format(dataEvento);
+		
+		//ritorno il ToString data formattata - titolo
+		return dataFormattata + "-" + titoloEvento;
+	}
 	
 	
 }
